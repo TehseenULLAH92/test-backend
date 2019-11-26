8264e6d9e0c338495fb865b1fa2ce45cfa613f56
Tehseen ULLAH <tehseenullah92@gmail.com>
2019-11-26 14:40:08 +0500

    oauth google strategy issue fixed

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,13 @@
-// const GooglePlusTokenStrategy = require ('passport-google-plus-token');
-// const passport = require('passport'); // Import passport
+const GooglePlusTokenStrategy = require ('passport-google-plus-token');
+const passport = require('passport'); // Import passport
 
-// // Google Oauth Strategy
-// passport.use('googleToken', new GooglePlusTokenStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   passReqToCallback: true
-// }, function(req, accessToken, refreshToken, profile, done) {
-//   console.log("accessToken", accessToken);
-//   console.log("refreshToken", refreshToken);
-//   console.log("profile", profile);
-// }));
+// Google Oauth Strategy
+passport.use('googleToken', new GooglePlusTokenStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  passReqToCallback: true
+}, function(req, accessToken, refreshToken, profile, done) {
+  console.log("accessToken", accessToken);
+  console.log("refreshToken", refreshToken);
+  console.log("profile", profile);
+}));
